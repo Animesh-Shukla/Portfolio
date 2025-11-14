245660c6392eb830edcf3d24ac283abb615ac72c
Animesh Shukla <animesh1836@gmail.com>
2025-11-14 19:29:31 +0530

    add support for receiving contact information

---
+++ src/main/java/com/animesh_shukla/portfolio/Model/Contact.java
@@ -0,0 +1,33 @@
+package com.animesh_shukla.portfolio.Model;
+
+public class Contact {
+    private String name;
+    private String email;
+    private String phone_number;
+    private String message;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone_number() {
+        return phone_number;
+    }
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
--- src/main/java/com/animesh_shukla/portfolio/PortfolioController.java
+++ src/main/java/com/animesh_shukla/portfolio/PortfolioController.java
@@ -2,7 +2,10 @@ package com.animesh_shukla.portfolio;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.ui.Model;
+import com.animesh_shukla.portfolio.Model.Contact;
 
 @Controller
 public class PortfolioController {
@@ -24,6 +27,22 @@ public class PortfolioController {
     public String showContactPage(Model model)
     {
         model.addAttribute("title", "Contact");
+        model.addAttribute("contact", new Contact());
+        return "main";
+    }
+
+    @PostMapping("/contact")
+    public String handleContactForm(@ModelAttribute Contact contactData, Model model)
+    {
+        System.out.println("Name: " + contactData.getName());
+        System.out.println("Email: " + contactData.getEmail());
+        System.out.println("Phone: " + contactData.getPhone_number());
+        System.out.println("Message: " + contactData.getMessage());
+        
+        // TODO: Save to database or send email
+        
+        model.addAttribute("title", "Contact");
+        model.addAttribute("message", "Thank you for contacting us!");
         return "main";
     }
 
@@ -33,6 +52,4 @@ public class PortfolioController {
         model.addAttribute("title", "Resume");
         return "main";
     }
-
-
 }
